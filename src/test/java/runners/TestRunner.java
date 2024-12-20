fd9b57edbbe635caa7b59a09de537849efef970d
Kiruba <mkirubagaran@griddynamics.com>
2024-12-20 19:22:20 +0530

    StepDefinitions file change

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -5,8 +5,6 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions
-        //(features = "src/test/resources/features/login.feature",
-        //glue = {"stepdefinitions", "utils","pages"},
         (dryRun = true, features = "src/test/resources/features",
                 glue = {"stepDefinitions","pages","utils"},
         plugin = {"pretty", "html:target/cucumber-reports.html"},
